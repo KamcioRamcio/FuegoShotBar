5a679200fa0f57715d40a1fd0b69dfae8aa235bc
kamcioramcio <kamilecki12@gmail.comgit config --global user.email kamilecki12@gmail.com>
2024-05-09 15:53:18 +0200

    burger-nav

--- src/components-en/Navbar-en.jsx
+++ src/components-en/Navbar-en.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import FUEGO from '../assets/MainPhotos/logo.png'
 import {motion} from 'framer-motion'
 import { Link } from 'react-scroll'
+import { useState } from "react";
+import menu from '../assets/MainPhotos/menu.svg'
+import close from '../assets/MainPhotos/close.svg'
 
 const NavbarEN = () => {
-
+  const [toggle, setToggle] = useState(false);
   const smoothScroll = (event, targetId) => {
     event.preventDefault(); 
     
@@ -22,6 +25,72 @@ const NavbarEN = () => {
 
   return (
     <div className='bg-slate-950 flex flex-row justify-between items-center fixed w-full z-50 nav'>
+        <div className='large:hidden pl-8'>
+          <img
+              src={toggle ? close : menu}
+              alt='menu'
+              className='w-[28px] h-[28px] object-contain'
+              onClick={() => setToggle((prev)=>!prev)}
+            />
+            <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } bg-slate-950 left-[10px] absolute top-20 my-2 min-w-[140px] z-10 rounded-xl text-s flex flex-col space-y-4 p-4`}
+            >
+              <motion.a href="#about" className='red_neon'
+            whileHover={{
+              scale: 1.2,
+              textShadow: "0px 0px 20px rgb(253, 78, 78)",
+            }}
+            onClick={(event) => smoothScroll(event, "about")}
+            >About Us
+            </motion.a>
+            <motion.a href="#places" className="yellow_neon"
+            whileHover={{
+              scale: 1.2,
+              textShadow: "0px 0px 20px rgb(253, 255, 20)",
+            }}
+            onClick={(event) => smoothScroll(event, "places")}
+            >Locations
+            </motion.a>
+            <motion.a href="#gallery" className='pink_neon'
+            whileHover={{
+              scale: 1.2,
+              textShadow: "0px 0px 20px rgb(255, 182, 193)",
+            }}
+            onClick={(event) => smoothScroll(event, "gallery")}
+            >Galerry
+            </motion.a>
+            <motion.a href="#contact" className='orange_neon'
+            whileHover={{
+              scale: 1.2,
+              textShadow: "0px 0px 20px rgb(243, 154, 52)",
+            }}
+            onClick={(event) => smoothScroll(event, "contact")}
+            >Contact
+            </motion.a>
+            <motion.a href="/join" className='purple_neon'
+            whileHover={{
+              scale: 1.2,
+              textShadow: "0px 0px 20px rgb(184, 93, 245)",
+            }}
+            onClick={(event) => smoothScroll(event, "join")}
+            >TeamFuego
+            
+            </motion.a>
+            <motion.a href="#franchise" className='cyan_neon'
+            whileHover={{
+              scale: 1.2,
+              textShadow: "0px 0px 20px rgb(59, 214, 235)",
+              
+            }}
+            onClick={(event) => smoothScroll(event, "franchise")}
+            >Franchise
+            </motion.a>
+            
+            </div>
+        </div>
+
         <Link to="home" className='l-tablet:w-full l-tablet:text-center l-tablet:block'>
             <img src={FUEGO} alt="FUEGO" className='FUEGO l-tablet:mx-auto' />
         </Link>
